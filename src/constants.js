be26bfe24666d07f28799631eec33a26f5531a87
Dzmitry Lapeta <dzmitry.lapeta@ibm.com>
2021-02-14 21:57:46 +0300

    Учебный проект: сервер наносит первый удар

--- src/constants.js
+++ src/constants.js
@@ -11,9 +11,18 @@ const ExitCode = {
 
 const maxOffersGenerate = 1000;
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
   ExitCode,
-  maxOffersGenerate
+  maxOffersGenerate,
+  HttpCode
 };
