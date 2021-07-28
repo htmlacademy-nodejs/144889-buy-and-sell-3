1817682f68e5d4b25a23c9154d6399bc11f825f9
Dzmitry Lapeta <Dzmitry.Lapeta@ibm.com>
2021-07-28 21:40:07 +0200

    Учебный проект: вспомнить всё

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -4,6 +4,7 @@ const express = require(`express`);
 const {HttpCode, API_PREFIX} = require(`../../constants`);
 const {getLogger} = require(`../lib/logger`);
 const routes = require(`../api`);
+const sequelize = require(`../lib/sequelize`);
 
 const DEFAULT_PORT = 3000;
 
@@ -12,6 +13,14 @@ const logger = getLogger({name: `api`});
 module.exports = {
   name: `--server`,
   async run(args) {
+    try {
+      logger.info(`Trying to connect to database...`);
+      await sequelize.authenticate();
+      logger.info(`The connection to database is established`);
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(1);
+    }
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
